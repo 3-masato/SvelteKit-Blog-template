957021d5ac4f6d09d460530fed87b2a8fe888070
3-masato <lzxqt0@gmail.com>
2024-01-21 18:00:46 +0900

    Fix: parseBlogContentのリファクタリング

--- src/lib/server/util/date.ts
+++ src/lib/server/util/date.ts
@@ -1,8 +1,24 @@
-import { format } from "date-fns";
+import type { BlogContent, FormatedDate } from "$types/blog";
+import { differenceInSeconds, format } from "date-fns";
 import { utcToZonedTime } from "date-fns-tz";
 
-export const formatDate = (date: string) => {
+const formatDate = (date: string) => {
 	const utcDate = new Date(date);
 	const jstDate = utcToZonedTime(utcDate, "Asia/Tokyo");
 	return format(jstDate, "yyyy/MM/dd");
+};
+
+export const formatDateInfo = ({
+	createdAt,
+	updatedAt,
+	publishedAt,
+	revisedAt
+}: BlogContent): FormatedDate => {
+	return {
+		createdDate: formatDate(createdAt),
+		updatedDate: formatDate(updatedAt),
+		publishedDate: formatDate(publishedAt),
+		revisedDate: formatDate(revisedAt),
+		isRevised: differenceInSeconds(new Date(publishedAt), new Date(revisedAt)) > 0
+	};
 };
--- src/lib/server/util/parse.ts
+++ src/lib/server/util/parse.ts
@@ -1,31 +1,20 @@
 import { getTocHeadings } from "$server/util/toc";
 import type { Blog, BlogContent } from "$types/blog";
 import * as cheerio from "cheerio";
-import { differenceInSeconds } from "date-fns";
-import { formatDate } from "./date";
+import { formatDateInfo } from "./date";
 import { applySyntaxHighlighting } from "./highlighter";
 
 export async function parseBlogContent(blog: BlogContent): Promise<Blog> {
-	const createdDate = formatDate(blog.createdAt);
-	const updatedDate = formatDate(blog.updatedAt);
-	const publishedDate = formatDate(blog.publishedAt);
-	const revisedDate = formatDate(blog.revisedAt);
-	const isRevised = differenceInSeconds(new Date(blog.publishedAt), new Date(blog.revisedAt)) > 0;
-
 	const $ = cheerio.load(blog.body);
 
 	const body = await applySyntaxHighlighting($);
 	const headingsArray = getTocHeadings($);
 
+	const formatedDate = formatDateInfo(blog);
+
 	return {
 		raw: blog,
-		date: {
-			createdDate,
-			updatedDate,
-			publishedDate,
-			revisedDate,
-			isRevised
-		},
+		date: formatedDate,
 		id: blog.id,
 		title: blog.title,
 		body: body,
