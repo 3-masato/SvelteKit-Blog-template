1e97dbe0466d581266d45627f33181ff411fa1c2
3-masato <lzxqt0@gmail.com>
2024-01-22 23:01:52 +0900

    Add: 更新日時コンポーネント追加

---
+++ src/lib/components/RevisedTime/RevisedTime.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { faClockRotateLeft } from "@fortawesome/free-solid-svg-icons";
+	import { Icon } from "svelte-awesome";
+
+	export let datetime: string;
+</script>
+
+<span class="inline-flex items-center gap-1 text-sm font-medium text-gray-500">
+	<Icon data={faClockRotateLeft} width={14} />
+	<time {datetime} itemprop="dateRevised">
+		<slot />
+	</time>
+</span>
