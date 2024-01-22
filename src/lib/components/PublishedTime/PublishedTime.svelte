aa787c531c75ad8bb476eb0f1ad9d343a950ba9f
3-masato <lzxqt0@gmail.com>
2024-01-22 23:02:09 +0900

    Add: 投稿日時コンポーネント追加

---
+++ src/lib/components/PublishedTime/PublishedTime.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { faClock } from "@fortawesome/free-regular-svg-icons";
+	import { Icon } from "svelte-awesome";
+
+	export let datetime: string;
+</script>
+
+<span class="inline-flex items-center gap-1 text-sm font-semibold text-gray-500">
+	<Icon data={faClock} width={16} />
+	<time {datetime} itemprop="datePublished">
+		<slot />
+	</time>
+</span>
