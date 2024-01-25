497970d23d9d6ba58aab830971f83103162ffef2
3-masato <lzxqt0@gmail.com>
2024-01-25 21:33:20 +0900

    Fix: テーマボタン微修正

--- src/lib/components/ThemeButton/ThemeButton.svelte
+++ src/lib/components/ThemeButton/ThemeButton.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { applyAction, enhance } from "$app/forms";
+	import IconButton from "$components/IconButton";
 	import { toggleTheme } from "$lib/util/theme";
 	import { theme } from "$stores/theme";
 	import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
-	import Icon from "svelte-awesome";
 
 	$: toggleThemeButtonIcon = $theme === "dark" ? faMoon : faSun;
 	$: nextTheme = toggleTheme($theme);
@@ -21,12 +21,5 @@
 	}}
 >
 	<input name="theme" value={nextTheme} hidden />
-	<button
-		aria-label="Toggle Theme Mode"
-		class="inline-flex h-8 w-8 flex-shrink-0 translate-y-0 items-center justify-center rounded-full font-medium text-neutral-800
-    transition-[transform,background-color] duration-300 ease-out hover:bg-neutral-500/30 active:translate-y-1
-    active:bg-neutral-500/15 active:duration-500 dark:text-neutral-200"
-	>
-		<Icon data={toggleThemeButtonIcon} />
-	</button>
+	<IconButton data={toggleThemeButtonIcon} ariaLabel="Toggle Theme Mode"></IconButton>
 </form>
