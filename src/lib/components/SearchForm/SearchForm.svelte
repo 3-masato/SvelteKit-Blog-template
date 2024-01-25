d8c6b92d7d2ae76382a8e62b6d016a50b9a87a87
3-masato <lzxqt0@gmail.com>
2024-01-25 21:26:18 +0900

    Fix: 検索フォームのロジック修正

--- src/lib/components/SearchForm/SearchForm.svelte
+++ src/lib/components/SearchForm/SearchForm.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import { afterNavigate, goto } from "$app/navigation";
+	import { afterNavigate } from "$app/navigation";
 	import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 	import { Icon } from "svelte-awesome";
+	import type { DOMAttributes } from "svelte/elements";
 
 	let searchValue = "";
-	const onSubmit = () => {
-		if (searchValue) {
-			goto(`/search?query=${encodeURIComponent(searchValue)}`);
+	const onSubmit: DOMAttributes<HTMLFormElement>["on:submit"] = (e) => {
+		if (!searchValue) {
+			return e.preventDefault();
 		}
 	};
 	afterNavigate(() => {
@@ -14,16 +15,20 @@
 	});
 </script>
 
-<form class="mx-4 flex grow items-center" on:submit|preventDefault={onSubmit}>
-	<div class="relative grow">
+<form method="GET" action="/search" class="contents" on:submit={onSubmit}>
+	<div class="relative">
 		<input
 			type="text"
-			class="h-10 rounded-sm bg-transparent px-4 py-2 ps-11"
-			placeholder="Search"
+			name="query"
+			class="w-full rounded-sm border-zinc-600 bg-transparent px-4 py-1 ps-11
+      placeholder:text-zinc-500 focus:border-blue-500 focus:ring-blue-500
+        disabled:pointer-events-none disabled:opacity-50
+      dark:border-zinc-600 dark:text-zinc-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+			placeholder={"Search"}
 			bind:value={searchValue}
 		/>
 		<div class="pointer-events-none absolute inset-y-0 start-0 z-20 flex items-center ps-4">
-			<Icon data={faMagnifyingGlass} />
+			<Icon data={faMagnifyingGlass} class="text-zinc-600 dark:text-zinc-500" />
 		</div>
 	</div>
 </form>
